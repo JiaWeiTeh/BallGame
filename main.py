00fb37e4c20b48daae011182244e8995c64126a5
JiaWeiTeh <teh_jiawei@yahoo.com>
2022-02-17 13:04:01 +0800

    Minor code fixes

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ ballvel = 1.2   #units/dt
 # timestep of the game
 dt = 1.5  #keep this constant, unless wanting to do slo/fast-mo
 # pause time before starting the game
-waittime = 20
+waittime = 50
 # total frame
 frames = 800
 
@@ -57,27 +57,23 @@ class Ball:
             
         # collisional area covered by the bar
         # techinically only need to care about the front side(?)
-        if (self.pos[0] >= barR.pos[0] - 3 and self.pos[0] < boardx) or\
-            (self.pos[0] <= barL.pos[0] + 3 and self.pos[0] > 0):
+        if (self.pos[0] >= barR.pos[0] - 3) or (self.pos[0] <= barL.pos[0] + 3):
                 # check if position of ball is within height of bar
-                if (self.pos[1] <= barR.pos[1] + barlength/2 and\
-                    self.pos[1] >= barR.pos[1] - barlength/2) or\
-                    (self.pos[1] <= barL.pos[1] + barlength/2 and\
-                    self.pos[1] >= barL.pos[1] - barlength/2):
+                if (self.pos[1] <= barR.pos[1] + barR.len/2 and\
+                    self.pos[1] >= barR.pos[1] - barR.len/2) or\
+                    (self.pos[1] <= barL.pos[1] + barL.len/2 and\
+                    self.pos[1] >= barL.pos[1] - barL.len/2):
                     # this will fix zigzag problem
                     if (self.pos[0] > boardx/2 and self.vel[0] > 0) or\
                         (self.pos[0] < boardx/2 and self.vel[0] < 0):
                         self.vel[0] = -self.vel[0]
-                        
-        """Problem now: ball will sometime go pass barR. Will sometimes also 
-        reflect eventhough clearly misses barL""""
-                
+
 class BarLeft:
     def __init__(self, position, length):
         self.pos = np.array(position).astype(float) # - length to shorten (?)
         self.len = length
         self.plt = np.array([[position[0], position[0]],\
-                             [position[1]-barlength/2, position[1]+barlength/2]])
+                             [position[1]-length/2, position[1]+length/2]])
         
     def update(self, dt):
         # If bar hits top/bottom after updating, don't update
@@ -119,7 +115,7 @@ class BarRight:
         self.pos = np.array(position).astype(float) # - length to shorten (?)
         self.len = length
         self.plt = np.array([[position[0], position[0]],\
-                             [position[1]-barlength/2, position[1]+barlength/2]])
+                             [position[1]-length/2, position[1]+length/2]])
         
     def update(self, dt):
         # If bar hits top/bottom after updating, don't update
@@ -189,19 +185,20 @@ def animate(i):
         # slowly ramp up ball velocity
         # if i%50 == 0:
             # ball.vel *= 1.5
+        # reset board if bars fail to catch ball
+        if ball.pos[0] > barR.pos[0] or ball.pos[0] < barL.pos[0]:
+            if (ball.pos[0] > boardx and (ball.pos[1] > barR.pos[1] or ball.pos[1] < barR.pos[1])) or\
+                (ball.pos[0] < 0 and (ball.pos[1] > barL.pos[1] or ball.pos[1] < barL.pos[1])):
+                anim.frame_seq = anim.new_frame_seq() 
+                init()
         ball.update(dt)
         barR.update(dt)
         barL.update(dt)
         plot_ball.set_data(ball.pos)
         plot_barL.set_data(barL.plt[0], barL.plt[1])
         plot_barR.set_data(barR.plt[0], barR.plt[1])
-    # reset board if bars fail to catch ball
-    if ball.pos[0] > barR.pos[0] or ball.pos[0] < barL.pos[0]:
-        if (ball.pos[0] > boardx and (ball.pos[1] > barR.pos[1] or ball.pos[1] < barR.pos[1])) or\
-            (ball.pos[0] < 0 and (ball.pos[1] > barL.pos[1] or ball.pos[1] < barL.pos[1])):
-            anim.frame_seq = anim.new_frame_seq() 
-            init()
     return plot_ball, plot_barR, plot_barL, plot_dash,
+        
 
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=frames, #change length of the animation
@@ -213,8 +210,8 @@ anim = animation.FuncAnimation(fig, animate, init_func=init,
 # =============================================================================
 # Save movie
 # =============================================================================
-# path2save = r"/Users/jwt/Documents/Code/Ball_Game/"
-# anim.save(path2save+'ball_game_stage6.mp4', fps=30,
-#           extra_args=['-vcodec', 'libx264'])
+path2save = r"/Users/jwt/Documents/Code/Ball_Game/"
+anim.save(path2save+'ball_game_stage6.mp4', fps=30,
+          extra_args=['-vcodec', 'libx264'])
 
 plt.show()
