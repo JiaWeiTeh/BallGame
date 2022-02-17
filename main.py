fc6b878b1f1688d9faff09435cef0e1b89df3e3d
JiaWeiTeh <teh_jiawei@yahoo.com>
2022-02-17 12:21:33 +0800

    Fixed zig-zaggy ball

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ barvel = .4   #units/dt
 # ball velocity
 ballvel = 1.2   #units/dt
 # timestep of the game
-dt = 1.5  #keep this constant, unless wanting to speed up time
+dt = 1.5  #keep this constant, unless wanting to do slo/fast-mo
 # pause time before starting the game
 waittime = 20
 # total frame
@@ -57,14 +57,20 @@ class Ball:
             
         # collisional area covered by the bar
         # techinically only need to care about the front side(?)
-        if (self.pos[0] >= barR.pos[0] - 4 and self.pos[0] < boardx) or\
-            (self.pos[0] <= barL.pos[0] + 4 and self.pos[0] > 0):
+        if (self.pos[0] >= barR.pos[0] - 3 and self.pos[0] < boardx) or\
+            (self.pos[0] <= barL.pos[0] + 3 and self.pos[0] > 0):
                 # check if position of ball is within height of bar
                 if (self.pos[1] <= barR.pos[1] + barlength/2 and\
                     self.pos[1] >= barR.pos[1] - barlength/2) or\
                     (self.pos[1] <= barL.pos[1] + barlength/2 and\
                     self.pos[1] >= barL.pos[1] - barlength/2):
-                    self.vel[0] = -self.vel[0]
+                    # this will fix zigzag problem
+                    if (self.pos[0] > boardx/2 and self.vel[0] > 0) or\
+                        (self.pos[0] < boardx/2 and self.vel[0] < 0):
+                        self.vel[0] = -self.vel[0]
+                        
+        """Problem now: ball will sometime go pass barR. Will sometimes also 
+        reflect eventhough clearly misses barL""""
                 
 class BarLeft:
     def __init__(self, position, length):
@@ -131,6 +137,9 @@ class BarRight:
         if ball.vel[0] < 0:
             return 
         
+        # Make bars shrink every 2 turns
+        # if dt%50 == 0: #extreme
+            # self.len -= 1
         # Now, make the bar chase the ball
         # Find difference in position
         pos_diff = ball.pos[1] - self.pos[1]
@@ -156,6 +165,7 @@ class BarRight:
     
 def init():
     global random_angle, ball, barR, barL
+    # random initial angle
     random_angle = np.deg2rad(np.random.choice([np.random.uniform(320, 400),\
                                      np.random.uniform(140, 220)]))
     ball = Ball([boardx/2,boardy/2],\
@@ -179,9 +189,6 @@ def animate(i):
         # slowly ramp up ball velocity
         # if i%50 == 0:
             # ball.vel *= 1.5
-        # Make bars shrink as time goes by
-        if i%50 == 0: #extreme
-            barR.len -= 1
         ball.update(dt)
         barR.update(dt)
         barL.update(dt)
@@ -191,7 +198,7 @@ def animate(i):
     # reset board if bars fail to catch ball
     if ball.pos[0] > barR.pos[0] or ball.pos[0] < barL.pos[0]:
         if (ball.pos[0] > boardx and (ball.pos[1] > barR.pos[1] or ball.pos[1] < barR.pos[1])) or\
-            (ball.pos[0] > boardx and (ball.pos[1] > barL.pos[1] or ball.pos[1] < barL.pos[1])):
+            (ball.pos[0] < 0 and (ball.pos[1] > barL.pos[1] or ball.pos[1] < barL.pos[1])):
             anim.frame_seq = anim.new_frame_seq() 
             init()
     return plot_ball, plot_barR, plot_barL, plot_dash,
@@ -204,7 +211,7 @@ anim = animation.FuncAnimation(fig, animate, init_func=init,
                                )
 
 # =============================================================================
-# Save
+# Save movie
 # =============================================================================
 # path2save = r"/Users/jwt/Documents/Code/Ball_Game/"
 # anim.save(path2save+'ball_game_stage6.mp4', fps=30,
